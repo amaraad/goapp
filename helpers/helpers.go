57a8e8e6999183dd5ff3d5ebb616fef7d669aa2b
Amaraa Baasanjav <amaraa.b@cumongol.mn>
2021-11-21 20:55:31 +0800

    feat: added helpers, migrations

---
+++ helpers/helpers.go
@@ -0,0 +1,40 @@
+package helpers
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+func HandleErr(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func HashAndSalt(pass []byte) string {
+	hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+	HandleErr(err)
+
+	return string(hashed)
+}
+
+
+func ValidateToken(id string, jwtToken string) bool {
+	cleanJWT := strings.Replace(jwtToken, "Bearer ", "", -1)
+	tokenData := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(cleanJWT, tokenData, func(token *jwt.Token) (interface{}, error) {
+		return []byte("TokenPassword"), nil
+	})
+	HandleErr(err)
+	var userId, _ = strconv.ParseFloat(id, 8)
+	if token.Valid && tokenData["user_id"] == userId {
+		return true
+	} else {
+		return false
+	}
+}
